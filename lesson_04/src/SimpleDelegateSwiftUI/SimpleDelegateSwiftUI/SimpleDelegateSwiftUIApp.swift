6a5d1ec8b08ca4866784c3a251418cd053a1559f
maxkalik <maxkalik@gmail.com>
2022-08-15 06:08:36 -0700

    Simple Delegate swiftUI

---
+++ lesson_04/src/SimpleDelegateSwiftUI/SimpleDelegateSwiftUI/SimpleDelegateSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDelegateSwiftUIApp.swift
+//  SimpleDelegateSwiftUI
+//
+//  Created by Maksim Kalik on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDelegateSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
